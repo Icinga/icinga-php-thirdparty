992fb6c5563ab85f893e2f109107f470904b3a71
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2025-11-28 11:34:21 +0000

    Version vphp-85

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'icinga/icinga-php-thirdparty',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1d15aea1737889233b84c26045c2ec524945eb55',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'icinga/icinga-php-thirdparty' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1d15aea1737889233b84c26045c2ec524945eb55',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
